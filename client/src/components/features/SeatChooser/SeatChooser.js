cf27ece584ca4b19a8234f93fb14f6cdffc6974d
dave8git <dave8git@gmail.com>
2023-01-23 12:37:14 +0100

    fix seatsLeft() not working

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -8,6 +8,7 @@ import socket from 'socket.io-client';
 
 const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   const [io, setIO] = useState(socket());
+  const [localSeats, setLocalSeats] = useState([]);
   const dispatch = useDispatch();
   const seats = useSelector(getSeats);
   const requests = useSelector(getRequests);
@@ -19,17 +20,26 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
   useEffect(() => { // jeżeli re-renderujesz komponent, to jeżeli io zmieniło swoją wartość wejdź do środka i odpal co jest w środku funkcji
       io.on('seatsUpdated', (payload) => {
         console.log('seatsUpdate', payload);
+        setLocalSeats(payload);
       });
   }, []);
 
+  useEffect(() => {
+    setLocalSeats(seats);
+  }, [seats])
+
   const isTaken = (seatId) => {
-    return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
+    return (localSeats.some(item => (item.seat === seatId && item.day === chosenDay)));
   }
 
   const prepareSeat = (seatId) => {
     if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
     else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
+  }
+
+  const seatsLeft = () => {
+    return seatsLeft.filter(seat => seat.day === chosenDay).filter(seat => seat.seat === chosenSeat); // .filter(jedna => funckja filtrująca).filter(druga => funkcja filtrująca)
   }
 
   return (
@@ -40,6 +50,7 @@ const SeatChooser = ({ chosenDay, chosenSeat, updateSeat }) => {
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
       { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+      <p>Seats available: { seatsLeft() }/50</p>
     </div>
   )
 }
