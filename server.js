6b0b8791031d5caa1c0044ad290c7fdb0bf0029c
dave8git <dave8git@gmail.com>
2023-01-28 21:38:35 +0100

    fix change database

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req, res) => {
 });
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/NewWaveDB', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
